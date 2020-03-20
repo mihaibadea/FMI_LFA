6defe98f9ea591f093328dc9cc237704964af238
Mihai Badea <mail@mihaibadea.ro>
2020-03-20 14:23:53 +0200

    Update Tema_1.cpp

--- Tema_1.cpp
+++ Tema_1.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-ifstream tin("autom.txt");
+ifstream tin("autom.in");
 
 vector<int> del[100][100];
 
@@ -13,9 +13,28 @@ map<char, int> trad;
 
 bool fin[100];
 
+int n;
+
+bool urm_lambda(int stare)
+{
+    return !del[stare][trad['$']].empty();
+}
+
+bool urm_alfa(int stare)
+{
+    for(int i=1; i<=n; i++)
+    {
+        if(!del[stare][i].empty()) return true;
+    }
+
+    return false;
+}
+
 bool evaluate(string k, int in)
 {
-    vector<int> sc, tmp;
+
+
+    vector<int> sc, tmp,a;
     sc.push_back(in);
     int lc = -1;
 
@@ -23,14 +42,14 @@ bool evaluate(string k, int in)
     {
         lc++;
 
-
         if(lc == k.length()-1)
         {
             for(int i=0; i<sc.size(); i++)
             {
                  for(int j=0; j<del[sc[i]][trad[k[lc]]].size(); j++)
                 {
-                    if(fin[del[sc[i]][trad[k[lc]]][j]]==1) return true;
+                    if(fin[del[sc[i]][trad[k[lc]]][j]]) return true;
+
                 }
             }
 
@@ -46,10 +65,34 @@ bool evaluate(string k, int in)
                 for(int j=0; j<del[sc[i]][trad[k[lc]]].size(); j++)
                 {
                     tmp.push_back(del[sc[i]][trad[k[lc]]][j]);
+
                 }
+
+                for(int j=0; j<del[sc[i]][trad['$']].size(); j++)
+                {
+                    tmp.push_back(del[sc[i]][trad['$']][j]);
+                }
+
             }
-            
+
+            int k=0;
+
+            while(k<tmp.size())
+            {
+                if(urm_lambda(tmp[k]))
+                {
+
+                    for(int j=0; j<del[tmp[k]][trad['$']].size(); j++) tmp.push_back(del[tmp[k]][trad['$']][j]);
+
+                    if(!urm_alfa(tmp[k])) tmp.erase(tmp.begin()+k);
+                    else k++;
+
+                }
+                else k++;
+            }
+
             sc = tmp;
+
         }
 
     }
@@ -61,7 +104,7 @@ bool evaluate(string k, int in)
 int main()
 {
 
-    int n,m,q0,k,l,t,a,c,in=-1;
+    int m,q0,k,l,t,a,c,in=0;
     char b,tt;
 
     tin>>n;
@@ -73,7 +116,7 @@ int main()
         if(t!='$') trad.insert(pair<char, int>(tt, ++in));
     }
 
-    trad.insert(pair<char, int>('$', -1));
+    trad.insert(pair<char, int>('$', 0));
 
     tin>>q0;
     tin>>k;
@@ -95,6 +138,7 @@ int main()
 
     int p=0;
     string test;
+
 
     while(1)
     {
